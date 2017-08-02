4d46e0c100ba4829d5f709508892e94185c152d2
Armen Badalyan <flexer87@gmail.com>
2017-08-02 18:05:26 +0300

    adding IndoorGeolocateControl

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const version: string = require('../package.json').version;
 const Map = require('./ui/map');
 const NavigationControl = require('./ui/control/navigation_control');
 const GeolocateControl = require('./ui/control/geolocate_control');
+const IndoorGeolocateControl = require('./ui/control/indoor_geolocate_control');
 const AttributionControl = require('./ui/control/attribution_control');
 const ScaleControl = require('./ui/control/scale_control');
 const FullscreenControl = require('./ui/control/fullscreen_control');
@@ -28,6 +29,7 @@ module.exports = {
     Map,
     NavigationControl,
     GeolocateControl,
+    IndoorGeolocateControl,
     AttributionControl,
     ScaleControl,
     FullscreenControl,
